646e5226330284bc9dce8a5f28ce3c7b0c4b7932
dariasergeevich <102729623+dariasergeevich@users.noreply.github.com>
2022-06-13 10:31:40 +0300

    подключен шрифт

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let editButton = document.querySelector('.profile__edit-btn');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-btn');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__info');
+let profileName = document.querySelector('.profile__name');
+let profileInfo = document.querySelector('.profile__description');
 
-//Открытие и закрытие попапа
 function popupInitial () {
     popup.classList.add ('popup_opend');
     console.log(popup.classList);
@@ -13,17 +17,6 @@ function popupClose () {
     console.log(popup.classList);
 };
 
-editButton.addEventListener('click', popupInitial);
-closeButton.addEventListener('click', popupClose);
-
-//Внесение данных в форму
-
-let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__info');
-let profileName = document.querySelector('.profile__name');
-let profileInfo = document.querySelector('.profile__description');
-
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы, логика отправки будет определена позже
 
@@ -31,5 +24,7 @@ profileName.textContent = `${nameInput.value}`;
 profileInfo.textContent = `${jobInput.value}`;
 }
 
+editButton.addEventListener('click', popupInitial);
+closeButton.addEventListener('click', popupClose);
 formElement.addEventListener('submit', formSubmitHandler); 
 formElement.addEventListener('submit', popupClose);
