695ed70d74605348272cb13e54fce989ea5d7256
dariasergeevich <102729623+dariasergeevich@users.noreply.github.com>
2022-06-14 22:47:45 +0300

    внесены исправления HTML и JS

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ let editButton = document.querySelector('.profile__edit-btn');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-btn');
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__info');
+let nameInput = document.getElementById('popupName');
+let jobInput = document.getElementById('popupInfo');
 let profileName = document.querySelector('.profile__name');
 let profileInfo = document.querySelector('.profile__description');
 
 function popupInitial () {
-    popup.classList.add ('popup_opend');
-    console.log(popup.classList);
+   nameInput.value = profileName.textContent;
+   jobInput.value = profileInfo.textContent;
+   popup.classList.add ('popup_opend');
 };
 
 function popupClose () {
     popup.classList.remove ('popup_opend');
-    console.log(popup.classList);
 };
 
 function formSubmitHandler (evt) {
@@ -22,9 +22,9 @@ function formSubmitHandler (evt) {
 
 profileName.textContent = `${nameInput.value}`;
 profileInfo.textContent = `${jobInput.value}`;
+popupClose();
 }
 
 editButton.addEventListener('click', popupInitial);
 closeButton.addEventListener('click', popupClose);
 formElement.addEventListener('submit', formSubmitHandler); 
-formElement.addEventListener('submit', popupClose);
